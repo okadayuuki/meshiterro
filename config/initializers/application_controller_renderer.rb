7e63b5e22eb1888fbda3f5e65909c614988cd2cb
@okadayuuki <oy19950908@gmail.com>
2020-07-30 20:25:55 +0900

    making

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6414300cf6a255d30385ef446dd3db88557baf5fa44fe7a74f8910b506e93e0fbd87e44a87288f06eb02316e6cff31c72750f46cb4a22293dc545e4dcde453fa'
+
